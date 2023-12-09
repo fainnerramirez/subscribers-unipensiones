68b211a50536a8e94fbc0db384f3092918a5d0e1
Fainner Ramirez <framirez@godoycordoba.com>
2023-12-09 18:25:58 -0500

    Update commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Tweet } from 'react-tweet';
 import { Box, Button } from '@chakra-ui/react';
 import HomePage from './pages/Home.page';
 import "./styles/global.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <Box className="App" bgGradient='linear(to-l, #F6FBFE, #E0E9F5)'>
       {/* <Tweet id={"1726105951005188343"} /> */}
       <HomePage />
+      <Analytics />
     </Box>
   );
 }
